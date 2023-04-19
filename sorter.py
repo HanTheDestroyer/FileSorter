894de3b9a3892528c7d6a9fbfeaa3a4191d0666f
HanTheDestroyer <123021973+HanTheDestroyer@users.noreply.github.com>
2023-04-19 22:21:26 +0300

    Create sorter.py

---
+++ sorter.py
@@ -0,0 +1,65 @@
+import os
+import datetime
+
+
+class Directory:
+    def __init__(self, file_type, location, extensions=()):
+        self.type = file_type
+        self.location = location
+        self.extensions = extensions
+
+
+def sort_folders():
+    # noinspection SpellCheckingInspection
+    image_extensions = (".jpg", ".jpeg", ".png", ".gif", ".bmp", ".ico", ".webp", ".tiff", ".svg", ".heif", ".apng",
+                        ".raw", ".indd", ".ai", ".eps", ".psd", ".pdd", ".xd", ".xcf")
+    # noinspection SpellCheckingInspection
+    video_extensions = (".mp4", ".avi", ".mkv", ".wmv", ".flv", ".mov", ".mpg", ".mpeg", ".m4v", ".3gp", ".webm",
+                        ".ogv", ".ts", ".mts", ".asf", ".rm", ".swf", ".vob", ".m2ts", ".divx")
+    # noinspection SpellCheckingInspection
+    audio_extensions = (".mp3", ".wav", ".aac", ".wma", ".m4a", ".flac", ".ogg", ".alac", ".aiff", ".opus", ".amr",
+                        ".au", ".cda", ".mid", ".midi", ".ra", ".dts", ".ac3", ".ape", ".cue")
+    # noinspection SpellCheckingInspection
+    doc_extensions = (".doc", ".docx", ".pdf", ".txt", ".rtf", ".xls", ".xlsx", ".csv", ".ppt", ".pptx", ".ods",
+                      ".odt", ".odp", ".xps", ".epub", ".pages", ".numbers", ".key", ".txt", ".md")
+    # noinspection SpellCheckingInspection
+    archive_extensions = (".zip", ".rar", ".tar", ".gz", ".bz2", ".7z", ".iso", ".dmg", ".cab", ".jar", ".ace", ".lzh",
+                          ".arj", ".gzip", ".uue", ".bzip2", ".z", ".war", ".ear", ".xar")
+    # noinspection SpellCheckingInspection
+    code_extensions = (".py", ".pyc", ".pyo", ".pyw", ".pyz", ".pyi", ".pyd", ".pyx", ".pxd", ".pxi", ".c", ".cpp",
+                       ".cc", ".cxx", ".h", ".hh", ".hpp", ".hxx", ".java", ".class", ".sh")
+    # noinspection SpellCheckingInspection
+    torrent_extensions = (".torrent", ".tor", ".t", ".tix", ".torrentpart")
+
+    # destinations
+    dir1 = Directory("image", r"C:\Users\Oguzhan\Images", image_extensions)
+    dir2 = Directory("video", r"C:\Users\Oguzhan\Videos", video_extensions)
+    dir3 = Directory("audio", r"C:\Users\Oguzhan\Audios", audio_extensions)
+    dir4 = Directory("doc", r"C:\Users\Oguzhan\Docs", doc_extensions)
+    dir5 = Directory("archive", r"C:\Users\Oguzhan\Archives", archive_extensions)
+    dir6 = Directory("code", r"C:\Users\Oguzhan\Codes", code_extensions)
+    destination_directories = [dir1, dir2, dir3, dir4, dir5, dir6]
+
+    # sources
+    dir7 = Directory("variety", r"C:\Users\Oguzhan\Desktop")
+    dir8 = Directory("variety", r"C:\Users\Oguzhan\Downloads")
+    source_directories = [dir7, dir8]
+
+    for source in source_directories:
+        files = os.listdir(source.location)
+        for file in files:
+            for destination in destination_directories:
+                if file.endswith(destination.extensions):
+                    try:
+                        os.rename(os.path.join(source.location, file), os.path.join(destination.location, file))
+                    except FileExistsError:
+                        new_file = "[" + \
+                                   str(datetime.datetime.now()).replace(":", "-").replace(" ", "-").replace(".", "-")\
+                                   + "]" + file
+                        os.rename(os.path.join(source.location, file), os.path.join(destination.location, new_file))
+            if file.endswith(torrent_extensions):
+                os.remove(os.path.join(source.location, file))
+
+
+if __name__ == "__main__":
+    sort_folders()
